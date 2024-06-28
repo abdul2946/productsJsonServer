0ba1eb1add5c0fd63854efe26945091ee04183a6
abdul2946 <79241859+abdul2946@users.noreply.github.com>
2024-06-28 12:35:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { create, router as _router, defaults } from "json-server";
+import { create, router, defaults } from "json-server";
 const server = create();
 
-const router = _router("products.json");
+const routes = router("products.json");
 
 const middlewares = defaults();
 
 const port = process.env.PORT || 8000
 
 server.use(middlewares);
-server.use(router);
+server.use(routes);
 server.listen(port);
