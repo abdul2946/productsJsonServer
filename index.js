7f98f5a7efa864f54b5ee00c2bb54d8b24d3ac92
abdul2946 <aazim3510@gmail.com>
2024-06-28 12:40:27 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 const jsonServer = require("json-server")
 const server = jsonServer.create();
-
 const router = jsonServer.router("products.json");
-=======
-import { create, router, defaults } from "json-server";
-const server = create();
-
-const routes = router("products.json");
->>>>>>> 0ba1eb1add5c0fd63854efe26945091ee04183a6
 
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8000
 
 server.use(middlewares);
-server.use(routes);
+server.use(router);
 server.listen(port);
